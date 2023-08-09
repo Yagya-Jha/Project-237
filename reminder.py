d150f1b6fece1830df3a2b235020966986a87c21
Yagya Jha <74773203+Yagya-Jha@users.noreply.github.com>
2023-08-09 16:57:51 +0530

    Initial commit

---
+++ reminder.py
@@ -0,0 +1,6 @@
+import time
+print("\t\t Timer for 30 mins to remind drinking water\t\t")
+print("\t\t (Will remind in 30s for debug purposes)")
+while True:
+    time.sleep(30)
+    print("Drink Water!!")
